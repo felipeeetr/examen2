7508ff9df83a86f182daeca3c8627d8a4e33259f
Felipe Torres <eftorres@poligran.edu.co>
2026-04-14 23:17:13 -0500

    VERSION INICIAL

---
+++ src/test/java/co/edu/poli/examen2_torres/AppTest.java
@@ -0,0 +1,20 @@
+package co.edu.poli.examen2_torres;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
